d1c1cce10b178fd2d4afc03a848affe954ad7dca
ManasaMona <manasaalisetty@gmail.com>
2017-02-02 13:01:55 +0530

    version 2

--- app/src/main/java/com/example/manasaa/task_fragment/MainActivity.java
+++ app/src/main/java/com/example/manasaa/task_fragment/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.manasaa.task_fragment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectFrag(View view) {
+        Fragment Frag;
+        if (view == findViewById(R.id.subButton)) {
+            Frag = new SubFragment();
+        }else {
+            Frag = new AddFragment();
+        }
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, Frag);
+        fragmentTransaction.commit();
+
+
     }
 }
